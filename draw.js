727f9f8730567db6617fd664c27ae9c5dc78055d
quantumpotato <quantumpotato@gmail.com>
2016-01-23 17:00:04 -0500

    Drawing fish tail from fish butt

--- draw.js
+++ draw.js
@@ -1,4 +1,14 @@
 Fish.prototype.draw = function(client, context) {
+	context.beginPath();
+	context.strokeStyle = "white";
+	context.ellipse(this.x, this.y, this.size * 2, this.size, 0, 10, 40, false);
 
+	var butt = {'x': this.x - this.size * 2, 'y' : this.y};
+	context.moveTo(butt.x, butt.y);
+	context.lineTo(butt.x - this.size, butt.y - this.size);
+	context.moveTo(butt.x, butt.y);
+	context.lineTo(butt.x - this.size, butt.y + this.size);
+  context.closePath();
+	context.stroke();
 };
 
--- game.js
+++ game.js
@@ -1,24 +1,6 @@
 "use strict";
 class Consumption extends Game {
-	
-	
-	
-	outOfBounds(group_name, thing) {
-		if (group_name == 'p1' || group_name == 'p2') {
-			var nose = thing.nosePoint();
-			var eng  = thing.enginePoint();
-			if (nose.x < 5 || eng.x < 5 || nose.x > this.canvas.width - 5 || eng > this.canvas.width - 5 || nose.y < 5 || eng.y < 5 || nose.y > this.canvas.height - 5 || eng > this.canvas.height - 5) {
-				return true;
-			}
-
-		}
- 		return super.outOfBounds(group_name, thing);
-	}
-
-	handleOutOfBounds(group_name, thing) {
-			thing.gone = true;
-	}
-	
+		
 	parsePlayerInput(player, left1, up1, right1, down1, firing1) {
 	}
 
@@ -28,7 +10,9 @@ class Consumption extends Game {
 	}
 
 	setupPlayers() {
-		this.fish = new Fish();
+		this.fish = new Fish({'position' : {'x': 150, 'y' : 150}});
+		this.add('fish', this.fish);
+		console.log(this.things['fish']);
 	}
 
 	resetGame() {
@@ -45,6 +29,7 @@ class Consumption extends Game {
 class Fish extends Thing {
 	constructor(options) {
 		super(options);
+		this.size = 40;
 	}
 
 }
